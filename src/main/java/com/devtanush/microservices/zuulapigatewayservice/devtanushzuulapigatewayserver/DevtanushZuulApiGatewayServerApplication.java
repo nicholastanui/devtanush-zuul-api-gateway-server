a90949c85277e47eda86c7fc14f93387d9bc4e7e
ntanui <ntanui@safaricom.co.ke>
2021-04-11 12:25:17 +0300

    init setup

---
+++ src/main/java/com/devtanush/microservices/zuulapigatewayservice/devtanushzuulapigatewayserver/DevtanushZuulApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.zuulapigatewayservice.devtanushzuulapigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtanushZuulApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevtanushZuulApiGatewayServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devtanush/microservices/zuulapigatewayservice/devtanushzuulapigatewayserver/DevtanushZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtanush.microservices.zuulapigatewayservice.devtanushzuulapigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtanushZuulApiGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
